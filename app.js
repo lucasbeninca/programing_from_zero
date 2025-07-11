f7c6f1e297f93b9486335a751d703f184534ba21
Lucas Beninca <lucasbeninca@proton.me>
2025-07-10 22:51:35 -0300

    número secreto

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-alert('ola mundo');
+alert('Boas vindas ao jogo do número secreto');
+let numeroSecreto = 5;
+let numeroEscolhido = prompt('Escolha um número entre 1 e 10');
+
+if(numeroSecreto == numeroEscolhido){
+    console.log('Isso ai! Você descobriu o número secreto')
+
+}else{
+    console.log('Não foi dessa vez o número secreto não é esse')
+}
