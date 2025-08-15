be0de45aae2efbb6abc8c0da4c86d03cde171739
Lucas <lucasbeninca@proton.me>
2025-08-14 22:56:22 -0300

    ternarios e Mathrandom

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 5;
-let numeroEscolhido = prompt('Escolha um número entre 1 e 10');
 
-if(numeroSecreto == numeroEscolhido){
-    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
-} else {
-    alert('Não foi dessa vez o número secreto não é esse');
+let numeroSecreto = parseInt(Math.random() * 10+1);;
+let numeroEscolhido = null;
+let tentativas = 1;
+
+
+
+// enquanto numero secreto for diferente do numero escolhido
+while (numeroSecreto != numeroEscolhido) {
+    numeroEscolhido = Number(prompt('Escolha um número entre 1 e 10'));
+    if (numeroSecreto == numeroEscolhido) {
+        break;
+    } else {  
+        if (numeroSecreto > numeroEscolhido) {
+            alert(`O número secreto é maior que ${numeroEscolhido}`);
+        } else {
+            alert(`O número secreto é menor que ${numeroEscolhido}`);
+        }
+        tentativas++;
+    }
 }
+
+//operador ternario
+let = palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa'
+alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com uma ${tentativas} ${palavraTentativa}`);
+
+
